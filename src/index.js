b5aef2d058bc3fadb2b67764e71193d1405e40cd
Shazad Hoang <shazad.hoang@gmail.com>
2020-12-20 14:31:02 +0100

    Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import store from "./store";
-import { bugAdded, bugResolved } from "./actions";
+import store from "./store/store";
+import { bugAdded, bugResolved } from "./store/actions";
 
-store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugAdded("Bug2"));
 store.dispatch(bugResolved(1));
 
 console.log(store.getState());
